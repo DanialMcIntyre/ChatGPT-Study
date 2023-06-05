9bc71591bc6b2643a288b919817f9ef950e8fbda
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2023-06-05 15:56:16 -0400

    Merged, updated README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ChatGPT-Study
 A python application that feeds ChatGPT your notes and it will give you many helpful studying resources.
+
+The application will read a PDF, and the user will have many options on what to do with it, such as creating Q-cards, summarizing it, or making a mock test based on a practice test.
+
+The GUI will be made using PyQT
+Basic tutorial: https://www.youtube.com/watch?v=c-g6epk3fFE
+Documentation: https://doc.qt.io/qtforpython-6/
+
+Uses the official ChatGPT API.
+Documentation: https://platform.openai.com/docs/introduction
+
+Made by Danial McIntyre and Iman Ullah
--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import openai
 from pdfminer.high_level import extract_text
-openai.api_key = "api"
+openai.api_key = ""
 
+#Reads pdf and returns string
 def extractPDF(fileName):
     text = extract_text(fileName)
     return text
 
 text = extractPDF("big.pdf")
 
-
-
+#Message to API
 completion = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
-    messages = [{"role": "user", "content" : "Create a 800 word summary as well as 10 questions with answers: \n" + text}]
+    messages = [{"role": "user", "content" : "How are you today?"}]
 )
 
+#Print output
 print(completion.choices[0].message.content)
 print(completion)
