7707e9b36802598b814522c7d0ce53d8670b69f6
Iman1121 <imanullah1112@gmail.com>
2023-06-05 15:30:36 -0400

    added pdf reader

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import openai
+from pdfminer.high_level import extract_text
+openai.api_key = "api"
 
-openai.api_key = "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"
+def extractPDF(fileName):
+    text = extract_text(fileName)
+    return text
+
+text = extractPDF("big.pdf")
+
+
 
 completion = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
-    messages = [{"role": "user", "content" : "Hi!"}]
+    messages = [{"role": "user", "content" : "Create a 800 word summary as well as 10 questions with answers: \n" + text}]
 )
 
+print(completion.choices[0].message.content)
 print(completion)
