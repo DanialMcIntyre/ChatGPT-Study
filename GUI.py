917cbf3eac267e99171ea738e3f45f08ea7dc88d
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2023-06-26 13:36:29 -0400

    PDFs can be added to GUI

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
-import sys
+import sys, os, shutil, pathlib
 from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QFileDialog, QTextEdit
+from PyQt6.QtGui import QAction, QIcon
 
 qtcreator_file  = "window.ui" # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
@@ -9,6 +11,29 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+        self.setWindowTitle("Study Assist")
+
+        self.showPDFs()
+        self.buttonAddPDF.clicked.connect(self.addPDF)
+
+    def addPDF(self):
+        #Select file
+        fname = QFileDialog.getOpenFileName(self, 'Open file', "${HOME}$", "PDF Files (*.pdf)")
+        #Copy file to pdfs folder
+        currDir = os.getcwd()
+        shutil.copy(fname[0], currDir + "/pdfs")
+        self.showPDFs()
+
+    def showPDFs(self):
+        self.listPDF.clear()
+        path = pathlib.Path('./pdfs')
+        for i, entry in enumerate(path.iterdir()):
+            if entry.is_file():
+                self.listPDF.insertItem(i, entry.name)
+        app.processEvents()
+
+        
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
