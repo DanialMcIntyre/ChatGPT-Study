c17ad15f4391523c0d055270511397670a7a88d9
Iman1121 <imanullah1112@gmail.com>
2023-06-26 14:07:44 -0400

    mock, summarize and qcard button prints to terminal

--- GUI.py
+++ GUI.py
@@ -2,6 +2,8 @@ import sys, os, shutil, pathlib
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtWidgets import QFileDialog, QTextEdit
 from PyQt6.QtGui import QAction, QIcon
+from test import extractPDF, createMockTest, createQCards, summarizePDF
+
 
 qtcreator_file  = "window.ui" # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
@@ -15,6 +17,9 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.showPDFs()
         self.buttonAddPDF.clicked.connect(self.addPDF)
+        self.buttonMock.clicked.connect(self.mockTest)
+        self.buttonCard.clicked.connect(self.qCards)
+        self.buttonSummarize.clicked.connect(self.summarize)
 
     def addPDF(self):
         #Select file
@@ -31,7 +36,21 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             if entry.is_file():
                 self.listPDF.insertItem(i, entry.name)
         app.processEvents()
+    
+    def mockTest(self):
+        text = self.listPDF.currentItem().text()
+        text = extractPDF("pdfs/"+text)
+        print(createMockTest(text))
 
+    def qCards(self):
+        text = self.listPDF.currentItem().text()
+        text = extractPDF("pdfs/"+text)
+        print(createQCards(text))
+
+    def summarize(self):
+        text = self.listPDF.currentItem().text()
+        text = extractPDF("pdfs/"+text)
+        print(summarizePDF(text))
         
 
 
